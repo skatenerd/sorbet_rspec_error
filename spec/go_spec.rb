a9fe90b0369832fe5e698ff442ba159c7ce2078d
Wai Lee Chin Feman <wailee.feman@gusto.com>
2022-09-06 10:52:58 -0400

    Show another way we can fix the issue

--- spec/go_spec.rb
+++ spec/go_spec.rb
@@ -3,6 +3,9 @@ require 'rspec'
 require 'go'
 
 describe Go do
+  before(:each) do
+    #T::Utils.signature_for_method(Go.method(:go))
+  end
   it 'can count the number of times the class method was called while invoking original code' do
     expect(Go).to receive(:go).and_call_original.twice
     Go.go
